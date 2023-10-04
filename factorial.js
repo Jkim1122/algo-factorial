1effb2c23da876df5959cad5aa1cee2523d33a4c
Jkim1122 <jkimz1122@gmail.com>
2023-10-04 15:45:25 -0500

    done w algo-fact

--- factorial.js
+++ factorial.js
@@ -1,5 +1,12 @@
-function factorial(num) {
-  return 0;
+const factorial = (num) => {
+  if (num === 1 || num === 0){
+    return 1;
+  } else return num * factorial(num - 1)
 }
-
 module.exports = factorial;
+
+console.log(factorial(2))
+console.log(factorial(3))
+console.log(factorial(4))
+console.log(factorial(8))
+console.log(factorial(18))
